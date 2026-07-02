0e6451b8ce0a99876e4a02d3fe6d3944e191bfaf
Kosta <goduskospan@gmail.com>
2026-07-02 13:58:00 +0500

    feat: setup project structure and generator configuration

---
+++ generator/config.py
@@ -0,0 +1,49 @@
+# количество пользователей и товаров
+NUM_USERS = 5000
+NUM_PRODUCTS = 1000
+
+# диапазоны дат
+# регистрация
+REGISTRATION_START = '2023-01-01'
+REGISTRATION_END = '2024-06-30'
+
+# ивенты - сессии, корзины, заказы, оплаты
+EVENT_START = '2023-01-01'
+EVENT_END = '2024-07-31'
+
+# списки категорий и подкатегорий
+CATEGORIES = ['Электроника', 'Одежда', 'Дом', 'Спорт', 'Красота']
+SUBCATEGORIES = {
+    'Электроника': ['Смартфоны', 'Ноутбуки']
+}
+
+# виды девайсов
+DEVICE_TYPES = ['desktop', 'mobile', 'tablet']
+
+# источники трафика
+UTM_SOURCES = ['organic', 'google_ads', 'facebook_ads', 'direct', 'referral']
+
+# вероятности
+CART_CREATION_PROBABILITY = 0.3 # создание корзины в сессии
+ORDER_PROBABILITY = 0.4         # оформление заказа
+PAYMENT_PROBABILITY = 0.9       # оплата заказа
+
+# мин макс цены товаров
+PRICE_MEAN = 200
+PRICE_STD = 100
+
+# мин макс товаров в корзине (уникальных товаров)
+CART_ITEM_MIN = 1
+CART_ITEM_MAX = 5
+
+# Название директории хранения сырых данных
+RAW_DATA_DIR = 'data/raw'
+
+# Названия файлов с сырыми данными
+USERS_FILE = 'users.csv'
+PRODUCTS_FILE = 'products.csv'
+SESSIONS_FILE = 'sessions.csv'
+CARTS_FILE = 'carts.csv'
+CART_ITEMS_FILE = 'cart_items.csv'
+ORDERS_FILE = 'orders.csv'
+PAYMENTS_FILE = 'payments.csv'
---
+++ generator/generate.py
@@ -0,0 +1,12 @@
+from generator.config import *
+import pandas as pd
+from faker import Faker
+
+
+def main():
+    print("Generating fake data")
+    # Здесь будет вызов генераторов данных
+
+
+if __name__ == '__main__':
+    main()
